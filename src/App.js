0b3322eecaa8c42ee5af5c7fd7008a6c3dd8da63
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-09-01 22:06:20 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dark Souls III</h1>
+      <h2>Firebase-based game wiki</h2>
+    </div>
+  );
+}
