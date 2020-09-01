05c2e2a73f9e3cdb91d60e162ff5d53f78bab060
Jose Hernandez <josemanuelhdez2@gmail.com>
2020-09-02 00:24:08 +0100

    icons for test cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
+import { Row, Card } from "react-bootstrap";
 import "./styles.css";
+
+import testData from "./data/test";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Dark Souls III</h1>
-      <h2>Firebase-based game wiki</h2>
+      <h2>game wiki</h2>
+      <Row>
+        {testData.items.map((it) => (
+          <Item item={it} />
+        ))}
+      </Row>
     </div>
   );
 }
+
+let Item = ({ item }) => (
+  <Card style={{ width: "18rem" }} key={item.id} bg="dark">
+    <Card.Body>
+      <Card.Title>{item.name}</Card.Title>
+      <img src={"image/" + item.img} width="100" alt="icon" />
+      <Card.Subtitle className="mb-2 text-muted">{item.type}</Card.Subtitle>
+      <Card.Text>{item.desc.replaceAll("\n\n", "...")}</Card.Text>
+    </Card.Body>
+  </Card>
+);
